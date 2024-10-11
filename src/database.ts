63c1e8a37b2852e9bbf69a66530c4f6e9ad17acc
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2024-10-11 12:27:11 +0900

    initial commit

---
+++ src/database.ts
@@ -0,0 +1,6 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGO_URL || "mongodb://0.0.0.0:27017";
+const client = new MongoClient(uri);
+client.connect();
+const db = client.db(process.env.DB_NAME || "keys-database");
